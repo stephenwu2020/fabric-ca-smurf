fbbc2a499cdd45f22c6ac9855683c7a7182761d4
stephenwu <stephenwu2020@protonmail.com>
2020-12-31 11:26:27 +0800

    before add element

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,6 +2,12 @@ module.exports = {
   pluginOptions: {
     autoRouting: {
       chunkNamePrefix: "page-"
+    },
+    i18n: {
+      locale: "en",
+      fallbackLocale: "en",
+      localeDir: "locales",
+      enableInSFC: false
     }
   }
 };
